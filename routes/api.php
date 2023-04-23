bace8b84ab90074e2ab3373b3e78f5a7e9048481
c0de4un <code4un@yandex.ru>
2023-04-23 09:09:50 +0400

    Register remaining auth endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\Auth\LoginController;
+use App\Http\Controllers\API\Auth\LogoutController;
+use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\Auth\TokenController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +21,11 @@ use App\Http\Controllers\API\Auth\LoginController;
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
         Route::post('/login', [LoginController::class, 'index']);
+        Route::post('/register', [RegisterController::class, 'index']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [LogoutController::class, 'index']);
+            Route::get('/token', [TokenController::class, 'index']);
+        });
     });
 });
-
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
