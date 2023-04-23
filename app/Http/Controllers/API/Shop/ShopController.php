80f6c79b00527a53da82d438734687326f2bb2d4
c0de4un <code4un@yandex.ru>
2023-04-23 14:42:07 +0400

    Add ShopController

---
+++ app/Http/Controllers/API/Shop/ShopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API\Shop;
+
+use App\Http\Controllers\API\ApiController;
+
+/**
+ * Class ShopController
+ * @package App\Http\Controllers\API\Shop
+ * @inheritDoc ApiController
+ */
+abstract class ShopController extends ApiController
+{
+}
