9ac71d41725bf63f7c6068c1e6ada23cec5ed3f8
c0de4un <code4un@yandex.ru>
2023-04-23 14:14:30 +0400

    Add admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Create Admin
+        User::create([
+            'login'    => 'admin',
+            'password' => bcrypt('12345678'),
+            'role'     => User::ROLE_ADMIN,
+            'balance'  => 0,
+        ]);
     }
 }
