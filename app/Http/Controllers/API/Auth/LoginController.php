099ccedf121eb6da9a13b1d00a2177354e40a6f1
c0de4un <code4un@yandex.ru>
2023-04-23 12:57:51 +0400

    Implement Login

--- app/Http/Controllers/API/Auth/LoginController.php
+++ app/Http/Controllers/API/Auth/LoginController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\API\Auth;
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 use App\Http\Requests\API\Auth\LoginRequest;
+use App\Models\User;
 
 /**
  * Class LoginController
@@ -18,7 +21,23 @@ final class LoginController extends AuthController
      */
     public function index(LoginRequest $request): JsonResponse
     {
+        /** @var User|null $user */
+        $user = User::where('login', '=', $request->login)
+            ->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'errors' => [
+                    'auth' => 'invalid login or password',
+                ],
+            ], 400);
+        }
+
+        $request->session()->regenerate();
+        Auth::login($user);
+
         return response()->json([
+            'balance' => $user->balance,
+            'token'   => $user->createToken('token')->plainTextToken,
         ]);
     }
 }
